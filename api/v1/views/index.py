dc68842d3dadc67761132179c604d24425a0f30f
Gloriana <141042662+Globski@users.noreply.github.com>
2024-08-05 11:27:16 -0700

    Add function to return the count of all class objects by type.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,3 +13,18 @@ def get_status():
     if request.method == 'GET':
         return jsonify({"status": "OK"})
 
+
+def get_status():
+    """
+    Function to return the count of all class objects by type.
+    """
+    if request.method == 'GET':
+        stats = {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)        
+        }
+        return jsonify(stats)
