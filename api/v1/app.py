bb22dcffaa48bfd26edf5cff232a5c4497a53aa8
Gloriana <141042662+Globski@users.noreply.github.com>
2024-08-05 11:48:00 -0700

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Flask App that serves as the backend API."""
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 import os
 
